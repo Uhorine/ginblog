ac8f52fa25bc69aaf38d8b19a37ee8b6e7c77802
Spxzx <Uhorine@gmail>
2022-06-04 16:49:42 +0800

    基本搭建

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"ginblog/model"
+	"ginblog/routes"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // 监听并在 0.0.0.0:8080 上启动服务
+	model.InitDb()
+	routes.InitRouter()
 }
